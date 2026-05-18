22baee38049401de3f0a5c469fa098ec2f252435
MailionDev <olatunjiayomi18@gmail.com>
2026-05-18 15:17:36 +0100

    adding local llm features

--- using_LLM_locally/local.py
+++ using_LLM_locally/local.py
@@ -1,10 +1,31 @@
 from langchain.chat_models import init_chat_model
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
 
 
 model = init_chat_model(
-    model="phi3:mini",
+    model="tinyllama",
     model_provider="ollama"
 )
+@app.route("/chat", methods=["POST"])
+def chat():
+    data = request.get_json()
+    response = model.invoke(data["inputs"])
 
-response = model.invoke("Is learnng Python in a year possible?")
-print(response.text)
+    return jsonify(
+        {"message": {
+            "role": "assistant",
+            "content": response.content
+        }}
+    )
+
+
+# response = model.invoke("Is learnng Python in a year possible?")
+# print(response.text)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5009) 
+
--- using_LLM_locally/note.py
+++ using_LLM_locally/note.py
@@ -6,15 +6,19 @@ We will be using the gpt-4.1-mini model, which is a smaller version of the GPT-4
 To install Ollama, you can follow the instructions on their website: https://ollama.com/docs/installation.
 
 WHen the ollama is installed, you can run the following command to pull the gpt-4.1-mini model:
-ollama pull gpt-4.1-mini
+    >> ollama pull gpt-4.1-mini
 After pulling the model, you can run the following command to start the Ollama server:
-ollama run phi3:mini 
+    >> ollama run phi3:mini 
+
+
 
 This model is a smaller version of the GPT-4 model, which is designed to be more efficient and 
 faster than the full GPT-4 model. It is suitable for tasks that do not require the full capabilities 
 of the GPT-4 model, such as simple text generation or basic question answering.
 
 
+Building API with Ollama and Langchain
+To build an API with Ollama and Langchain, we can use the following code:
 
 
 '''
